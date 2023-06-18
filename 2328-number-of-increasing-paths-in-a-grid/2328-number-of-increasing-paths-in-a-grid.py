4e7314ecb576e6e8aa39a4b3636fcc5b71c8b9f5
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2023-06-18 00:18:18 -0700

    Time: 2421 ms (52.43%), Space: 119.7 MB (20.88%) - LeetHub

---
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPaths(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        mod = 10**9+7
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        
+        result = 0
+        
+        @cache
+        def dfs(i,j):
+            
+            res = 0
+            
+            for dx,dy in directions:
+                if (0 <= i+dx < m) and (0 <= j+dy < n) and (grid[i+dx][j+dy] > grid[i][j]):
+                    res += (1 + dfs(i+dx,j+dy))
+        
+            return res
+        
+        for i in range(m):
+            for j in range(n):
+                result = (result+ (dfs(i,j)%mod))%mod
+        
+        return result+m*n
