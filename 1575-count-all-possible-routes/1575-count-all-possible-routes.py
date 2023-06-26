54d77918fc3e3e5f4c6a976282382ebfbdc40e5e
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2023-06-25 18:28:58 -0700

    Time: 7907 ms (5.68%), Space: 17.4 MB (86.52%) - LeetHub

---
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
+        MOD = 1000000007
+        n = len(locations)
+        
+        dp = [[0] * (fuel + 1) for _ in range(n)]
+        for f in range(fuel + 1):
+            dp[finish][f] = 1
+        
+        for f in range(fuel + 1):
+            for city in range(n):
+                for nextCity in range(n):
+                    if nextCity != city and f >= abs(locations[nextCity] - locations[city]):
+                        dp[city][f] = (dp[city][f] + dp[nextCity][f - abs(locations[nextCity] - locations[city])]) % MOD
+        
+        return dp[start][fuel]
