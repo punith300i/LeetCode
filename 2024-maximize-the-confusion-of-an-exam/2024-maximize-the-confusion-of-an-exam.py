84b3ed2bbd3b06627e63c38c0f66205bfc939e6e
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2023-07-07 16:51:17 -0700

    Time: 574 ms (15.97%), Space: 16.7 MB (86.45%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -1,15 +1,19 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         
-        l,r = 0,0
-        counter = {}
-        res = 0
-        while r < len(answerKey):
-            counter[answerKey[r]] = counter.get(answerKey[r],0)+1
-            diff = r-l+1 - counter[max(counter,key=counter.get)]
-            if diff > k:
-                counter[answerKey[l]]-=1
-                l+=1
-            res = max(res, r-l+1)
-            r+=1
-        return res
+        def replace_lit(lit):
+            l = 0
+            counter = defaultdict(int)
+            res = 0
+            for r in range(len(answerKey)):
+                counter[answerKey[r]]+=1
+                
+                while counter[lit]>k:
+                    counter[answerKey[l]]-=1
+                    l+=1
+                
+                res = max(res,r-l+1)
+            
+            return res
+        
+        return max(replace_lit('F'), replace_lit('T'))
