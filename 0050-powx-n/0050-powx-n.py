e8b172ab9e00b6f0f99c8176f6e93a771009efdc
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2023-07-24 10:51:57 -0700

    Time: 40 ms (89.54%), Space: 16.5 MB (7.14%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,10 @@
+class Solution:
+    @cache
+    def myPow(self, x: float, n: int) -> float:
+        if n==0:
+            return 1
+        if n==1:
+            return x
+        if n==-1:
+            return 1/x
+        return self.myPow(x, n//2) * self.myPow(x, n//2) * self.myPow(x, n%2)
