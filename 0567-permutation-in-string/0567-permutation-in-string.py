f8956b45df74fe7d009134473c4b6c286733b45a
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2023-09-01 15:51:18 -0700

    Time: 2305 ms (16.70%), Space: 16.5 MB (55.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,29 +1,7 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
-        def counterCheck(string1: str, string2: str) -> bool:
-            dict1 = {}
-            
-            for i in string1:
-                if i not in dict1:
-                    dict1[i] = 1
-                else:
-                    dict1[i]+=1
-            
-            for i in string2:
-                if i not in dict1.keys():
-                    return False
-                else:
-                    dict1[i]-=1
-            
-            for i in dict1.values():
-                if i!=0:
-                    return False
-            
-            return True
-        
         for i in range(len(s2) - len(s1)+1):
-            if counterCheck(s1,s2[i:i+len(s1)]):
+            if Counter(s1) == Counter(s2[i:i+len(s1)]):
                 return True
         
         return False
