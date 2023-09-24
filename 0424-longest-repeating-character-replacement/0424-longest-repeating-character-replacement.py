699d599bd06325910a2c64247677c811edfe809a
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2023-09-24 16:29:10 -0700

    Time: 237 ms (9.62%), Space: 16.4 MB (68.14%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l=r=0
+        c = Counter()
+        res = 0
+        for r in range(len(s)):
+            c[s[r]]+=1
+            if r-l+1 - c.most_common(1)[0][1] <=k:
+                res = r-l+1
+            else:
+                c[s[l]]-=1
+                l+=1
+                
+        return res
