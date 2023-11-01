58d06dd9c1e5a2072c4a444e4808ec8d90ee8836
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2023-11-01 15:45:34 -0700

    Time: 475 ms (76.85%), Space: 22.1 MB (75.32%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -2,7 +2,6 @@ class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         low = max(weights)
         high = sum(weights)
-        result = low
         while(low<=high):
             mid = low + (high - low)//2
             mid_req_days = 0
@@ -16,8 +15,7 @@ class Solution:
             mid_req_days+=1            
             if mid_req_days <= days:
                 high = mid-1
-                result = mid
             else:
                 low = mid+1
                 
-        return result
+        return low
