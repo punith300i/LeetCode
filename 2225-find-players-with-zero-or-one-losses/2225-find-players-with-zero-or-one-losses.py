8873bee472aa5fc97747499aae5017dd98d89eab
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-01-14 16:57:04 -0800

    Time: 1392 ms (92.18%), Space: 72.4 MB (21.70%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,11 +1,12 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         answer = [0,0]
+        
         winners = set(list(map(lambda x : x[0], matches)))
         losers = set(list(map(lambda x : x[1], matches)))
+        l_counters = Counter(list(map(lambda x : x[1], matches)))
         
         answer[0] = sorted(list(winners-losers))
-        l_counters = Counter(list(map(lambda x : x[1], matches)))
         answer[1] = sorted([k for k,v in l_counters.items() if v==1])
         
         return answer
