09963a002509dc5adb35ccbdb37db8668aaac798
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-02-02 18:33:15 -0800

    Time: 274 ms (94.22%), Space: 16.6 MB (84.70%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,17 +1,8 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
-        dp = [-1]*n
-        def recursive_function(index: int) -> any:
-            
-            # base case
-            if index == n:
-                return 0
-            
-            # memoization
-            if dp[index] != -1:
-                return dp[index]
-                
+        dp = [0]*(n+1)
+        for index in range(n)[::-1]:
             length = 0
             subarray_maximum = float('-inf')
             maximum_sum = float('-inf')
@@ -19,10 +10,9 @@ class Solution:
             for j in range(index, min(index+k,n)):
                 length+=1
                 subarray_maximum = max(subarray_maximum, arr[j])
-                sum_so_far = length * subarray_maximum + recursive_function(j+1)
+                sum_so_far = length * subarray_maximum + dp[j+1]
                 maximum_sum = max(maximum_sum, sum_so_far)
             
             dp[index] = maximum_sum
-            return dp[index]
+        return dp[0]
         
-        return recursive_function(0)
