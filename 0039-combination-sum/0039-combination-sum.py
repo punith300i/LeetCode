50ddf9a12860d92bb3b2441a258303734a184f85
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-02-05 15:28:55 -0800

    Time: 52 ms (70.82%), Space: 16.7 MB (66.51%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        def rec_func(index,lst,s):
+            nonlocal res
+            
+            if s > target:
+                return
+            
+            if index == len(candidates):
+                if s==target:
+                    res.append(lst.copy())
+                    return
+                else:
+                    return
+            
+            lst.append(candidates[index])
+            rec_func(index, lst, s+candidates[index])
+            lst.pop()
+            rec_func(index+1, lst, s)
+        
+        rec_func(0,[],0)
+        return res
