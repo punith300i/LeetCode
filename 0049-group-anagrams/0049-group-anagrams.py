fb96e6276e8d18d4ed69a647bff6d913badd85cb
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-02-05 17:14:00 -0800

    Time: 91 ms (61.23%), Space: 20.9 MB (43.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_strs = [''.join(sorted(str_val)) for str_val in strs]
-        smaps = {}
         result = []
-        for i,j in zip(sorted_strs,strs):
-            if i not in smaps:
-                smaps[i] = [j]
-            else:
-                smaps[i].append(j)
-        for i,j in smaps.items():
-            result.append(j)
-        return result
+        sorted_strings = list(enumerate([(''.join(sorted(string))) for string in strs]))
+        hashmap = defaultdict(list)
+        for idx, string in sorted_strings:
+            hashmap[string].append(strs[idx])
+        return hashmap.values()
+        
