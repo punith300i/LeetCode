e3de566173c89be3d57df8a2b3b98ae6fb1bd745
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-02-10 13:49:03 -0800

    Time: 42 ms (33.34%), Space: 16.5 MB (64.21%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -2,17 +2,17 @@ class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
         
-        def rec_func(index, lst, s):
-            if s == 0 and len(lst) == k:
-                res.append(lst)
-                return
+        def rec_func(index, lst, target):
+            if target == 0 and len(lst)==k:
+                res.append(lst.copy())
             
-            if index>=n or index>=10:
-                return
             
-            if index<=s:
-                rec_func(index+1, lst+(index,), s-index)
-            rec_func(index+1, lst, s)
+            for i in range(index,10):
+                if i<=target:
+                    lst.append(i)
+                    rec_func(i+1, lst, target-i)
+                    lst.pop()
+                
         
-        rec_func(1,tuple(),n)
+        rec_func(1,[],n)
         return res
