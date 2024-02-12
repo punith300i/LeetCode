8e9942f846723bc05031b75fdfb21fe303100ef5
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-02-11 23:06:48 -0800

    Time: 200 ms (68.10%), Space: 23.4 MB (57.17%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,20 +2,22 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         dp = [[-1 for i in range(sum(nums)*2 + 1)] for i in range(len(nums))]
         def rec_func(index,s):
-            if index == len(nums):
-                if s == target:
+            
+            if index < 0 and s == target:
                     return 1
-                else:
+                
+            if index < 0:
                     return 0
             
             if dp[index][s]!=-1:
                 return dp[index][s]
             
-            pos = rec_func(index+1, s + nums[index])
-            neg = rec_func(index+1, s + (-1 * nums[index]))
+            
+            pos = rec_func(index-1, s + nums[index])
+            neg = rec_func(index-1, s + (-1 * nums[index]))
             
             dp[index][s] = pos+neg
             
             return dp[index][s]
         
-        return rec_func(0,0)
+        return rec_func(len(nums)-1,0)
