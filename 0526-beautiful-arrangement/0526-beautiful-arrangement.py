0ace3fcc358cfe74c8d68dc2fa419cf8de736119
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-02-12 00:21:23 -0800

    Time: 98 ms (88.09%), Space: 25.6 MB (5.09%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -2,17 +2,24 @@ class Solution:
     def countArrangement(self, n: int) -> int:
         res = 0
         freq = [0 for i in range(n+1)]
-        def rec_func(index):
+        dp = {}
+        def rec_func(index,freq):
             if index == n+1:
-                nonlocal res
-                res+=1
-                return
+                return 1
             
+            #memo :
+            if (index, tuple(freq)) in dp:
+                return dp[(index, tuple(freq))]
+            
+            count = 0
             for i in range(1,n+1):
                 if not freq[i]:
                     if i%index == 0 or index%i==0:
                         freq[i]=1
-                        rec_func(index+1)
+                        count+=rec_func(index+1,freq)
                         freq[i]=0
-        rec_func(1)
-        return res
+                        
+            dp[(index, tuple(freq))] = count
+            return count
+        
+        return rec_func(1,freq)
