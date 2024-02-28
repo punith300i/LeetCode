8ec75f72bd90c5ef365205502029eb7125dbaa9a
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-02-27 19:14:42 -0800

    Time: 44 ms (46.23%), Space: 18.2 MB (97.42%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -6,17 +6,14 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        def level_order_traversal(root):
-            queue = deque()
-            queue.append(root)  
-            while queue:
-                res = queue[0].val
-                for i in range(len(queue)):
-                    temp = queue.popleft()
-                    if temp.left is not None:
-                        queue.append(temp.left)
-                    if temp.right is not None:
-                        queue.append(temp.right)
-            return res
-        
-        return level_order_traversal(root)
+        queue = deque()
+        queue.append(root)  
+        while queue:
+            res = queue[0].val
+            for i in range(len(queue)):
+                temp = queue.popleft()
+                if temp.left:
+                    queue.append(temp.left)
+                if temp.right:
+                    queue.append(temp.right)
+        return res
