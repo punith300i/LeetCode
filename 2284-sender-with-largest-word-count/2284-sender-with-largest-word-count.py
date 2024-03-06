920d6ab7e928e09746ac1fb677d854a15ce2a4d3
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-03-06 13:19:52 -0800

    Time: 327 ms (87.46%), Space: 24.1 MB (34.17%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
@@ -5,5 +5,5 @@ class Solution:
             hmap[senders[i]] = hmap.get(senders[i],0)+len(messages[i].split())
         max_val = max(hmap.values())
         max_val_senders = [k for k,v in hmap.items() if v == max_val]
-        return sorted(max_val_senders)[-1]
+        return max(max_val_senders)
             
