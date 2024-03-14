1cee95673cb3576920ee65639e5deca5e6973e91
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-03-14 00:53:52 -0700

    Time: 18 ms (100.00%), Space: 7.3 MB (100.00%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.c
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.c
@@ -1,23 +1,23 @@
-func helper(nums []int, x int) int {
-    
-    if x<0 {
-        return 0
+int helper(int *nums, int goal, int n){
+    if(goal<0){
+        return 0;
     }
     
-    var l int = 0
-    var res int = 0
-    var current_val int = 0
-    for r, ele := range nums{
-        current_val = current_val + ele
-        for current_val > x {
-            current_val = current_val - nums[l]
-            l = l + 1
+    int l = 0;
+    int res = 0;
+    int cur_val = 0;
+    for(int r=0; r<n; r++){
+        cur_val = cur_val + nums[r];
+        while(cur_val > goal){
+            cur_val = cur_val - nums[l];
+            l = l + 1;
         }
-        res = res + r-l + 1
+        res = res+(r-l+1);
     }
-    return res
+    return res;
 }
 
-func numSubarraysWithSum(nums []int, goal int) int {
-    return helper(nums, goal) - helper(nums, goal-1)
+int numSubarraysWithSum(int* nums, int numsSize, int goal){
+    
+    return helper(nums, goal, numsSize) - helper(nums,goal-1, numsSize);
 }
