5e0d137ff3c2c829264172d451c6b576a5538398
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-03-18 23:05:22 -0700

    Time: 0 ms (100.00%), Space: 6.2 MB (87.18%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,22 +1,38 @@
-class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        counts = Counter(tasks)
-        heap = [-cnt for cnt in counts.values()]
-        time = 0
-        heapq.heapify(heap)
-        q = deque()
-        
-        while heap or q:
-            time+=1
-            
-            if heap:
-                cnt = 1+heapq.heappop(heap)
-                if cnt:
-                    q.append((time, cnt))
-            
-            if q and q[0][0] + n == time:
-                heapq.heappush(heap, q.popleft()[1])
-        
-        return time
-            
-            
+func leastInterval(tasks []byte, n int) int {
+    // compute frequency array
+    freq := make([]int, 26)
+    for _, task := range tasks{
+        freq[task -'A']++
+    }
+    
+    // sort frequencies
+    sort.Ints(freq)
+    
+    // most frequent task
+    max_freq := freq[len(freq)-1]
+    
+    // no of idle slots = no of most freq task - 1 (as we dont have idle states in the end) * n
+    idle_slots := (max_freq - 1) * n
+    
+    // fill the idle slots with remaining available tasks
+    for i:=len(freq)-2;i>=0 && idle_slots>0;i--{
+        idle_slots -= min_val(freq[i], max_freq-1)
+    }
+    
+    // if there are idle slots present then add it to the final result
+    if idle_slots>0{
+        return len(tasks) + idle_slots
+    }else{
+        return len(tasks)
+    }
+}
+
+
+// helper function to compute min value
+func min_val(i,j int) int{
+    if i<=j{
+        return i
+    }else{
+        return j
+    }
+}
