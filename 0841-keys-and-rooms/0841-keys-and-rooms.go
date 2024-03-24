9e8921b7e1350ccdc1294129bafd3e4bede592c0
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-03-24 01:56:46 -0700

    Time: 5 ms (67.40%), Space: 3.8 MB (49.75%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.go
+++ 0841-keys-and-rooms/0841-keys-and-rooms.go
@@ -1,18 +1,13 @@
 func canVisitAllRooms(rooms [][]int) bool {
     visited := map[int]bool{}
-    dfs(0, visited,rooms)
-    for i := range rooms{
-        if _, ok := visited[i]; !ok {
-            return false
-        }
-    }
-    return true
+    dfs(0, visited, &rooms)
+    return len(visited) == len(rooms)
     
 }
 
-func dfs(start int, visited map[int]bool, rooms[][]int){
+func dfs(start int, visited map[int]bool, rooms *[][]int){
     visited[start] = true
-    for _, key := range rooms[start]{
+    for _, key := range (*rooms)[start]{
         if _, ok := visited[key]; !ok{
             dfs(key,visited,rooms)
         }
