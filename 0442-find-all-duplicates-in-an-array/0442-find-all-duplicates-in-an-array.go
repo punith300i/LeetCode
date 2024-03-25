45817ddc07fd1902337c46a0887a847e4944d29d
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-03-24 18:32:35 -0700

    Time: 38 ms (90.63%), Space: 7.1 MB (78.13%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.go
@@ -0,0 +1,19 @@
+func findDuplicates(nums []int) []int {
+    var result []int = []int{}
+    
+    for _, num := range nums{
+        index := abs(num) - 1
+        if nums[index]<0 {
+            result = append(result, abs(num))
+        }
+        nums[index] = -nums[index]
+    }
+    
+    return result
+}
+func abs(value int) int {
+    if value < 0{
+        return -value
+    }
+    return value
+}
