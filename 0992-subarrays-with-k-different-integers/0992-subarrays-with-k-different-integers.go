b226bce733b07bd711bc66c5cdb2b7c9e9ec9b5b
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-03-29 22:58:04 -0700

    Time: 66 ms (75.00%), Space: 7.1 MB (50.00%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.go
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.go
@@ -1,21 +1,21 @@
 func subarraysWithKDistinct(nums []int, k int) int {
     // to get exactly K, we can subtract at most k value with the at most k-1 value
-    return atMostK(nums, k) - atMostK(nums,k-1)
+    return atMostK(&nums, k) - atMostK(&nums,k-1)
 }
 
-func atMostK(n []int, k int) int{
+func atMostK(n *[]int, k int) int{
     var hmap = map[int]int{}
     left, count, res := 0, 0, 0
-    for right:=0; right<len(n); right++{
+    for right:=0; right<len(*n); right++{
         // if n[right] is not visited
-        if hmap[n[right]] == 0{
+        if hmap[(*n)[right]] == 0{
             count++
         }
-        hmap[n[right]]++
+        hmap[(*n)[right]]++
         for count > k{
-            hmap[n[left]]--
+            hmap[(*n)[left]]--
             // if nums[left] is removed and only one if it is present
-            if hmap[n[left]] == 0{
+            if hmap[(*n)[left]] == 0{
                 count--
             }
             left++
