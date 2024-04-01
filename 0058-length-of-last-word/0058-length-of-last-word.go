30394c1caf0621b8889236d85efd69dc0a30124c
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-03-31 21:02:46 -0700

    Time: 0 ms (100.00%), Space: 2.4 MB (28.26%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.go
@@ -0,0 +1,12 @@
+func lengthOfLastWord(s string) int {
+    var res int = 0
+    for i:=len(s)-1; i>-1; i--{
+        if string(s[i])!=" "{
+            res++
+            if i-1 > -1 && string(s[i-1]) == " "{
+                break
+            }
+        }
+    }
+    return res
+}
