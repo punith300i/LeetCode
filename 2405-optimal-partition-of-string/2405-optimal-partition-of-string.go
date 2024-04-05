e806593d375115a14105d58b124e40d1b5e449e2
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-04-04 23:01:02 -0700

    Time: 60 ms (16.56%), Space: 7.3 MB (16.56%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.go
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.go
@@ -1,10 +1,10 @@
 func partitionString(s string) int {
-    hmap := map[byte]bool{}
+    hmap := make(map[byte]bool)
     ans := 1
     for i:=0; i<len(s); i++{
         if hmap[s[i]]{
             ans++
-            hmap = map[byte]bool{}
+            hmap = make(map[byte]bool)
         }
         hmap[s[i]] = true
     }
