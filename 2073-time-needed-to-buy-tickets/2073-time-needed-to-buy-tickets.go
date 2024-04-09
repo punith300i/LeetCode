12ab4d87e24479a46633c70afb569981220a4cab
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-04-08 22:22:26 -0700

    Time: 3 ms (7.69%), Space: 4.6 MB (7.69%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.go
@@ -0,0 +1,28 @@
+func timeRequiredToBuy(tickets []int, k int) int {
+    queue := make([]int, 0)
+    counts := 0
+    
+    for i := range tickets{
+        queue = append(queue, i)
+    }
+    
+    fmt.Println(queue)
+    
+    for len(queue)>0 {
+        idx := queue[0]
+        queue = queue[1:]
+        
+        tickets[idx]--
+        counts++
+        
+        if tickets[idx] == 0 && idx == k {
+            return counts
+        }
+        
+        if tickets[idx] > 0{
+            queue = append(queue, idx)
+        }
+        
+    }
+    return counts
+}
