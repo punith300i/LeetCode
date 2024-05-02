80ca2235aa080a49244a78e46ea4f11f94d380fd
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2024-05-01 22:58:25 -0700

    Time: 16 ms (61.54%), Space: 6.8 MB (30.77%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.go
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.go
@@ -1,15 +1,19 @@
 func findMaxK(nums []int) int {
-    var maxVal int = -1
     numsMap := make(map[int]bool)
     for _, val := range(nums){
         if _, ok := numsMap[val]; !ok{
             numsMap[val]=true
         }
     }
-    for _, val := range(nums){
-        if val > 0 && numsMap[-val] && val > maxVal{
-            maxVal = val
+    
+    sort.Slice(nums, func (i,j int) bool {
+        return nums[i] > nums[j]
+    })    
+    
+    for i:=len(nums)-1; i>=0; i--{
+        if numsMap[-nums[i]]{
+            return -nums[i]
         }
     }
-    return maxVal
+    return -1
 }
